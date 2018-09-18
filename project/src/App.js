5fe7f850714e6835be141d36fea30ebcfd5671b6
Michelle Poterek <poterekm@gmail.com>
2018-09-17 20:38:48 -0700

    console log for fetch works with github api
    I do not have any "gists" so the API test call with my username shows up as an empty array.

--- project/src/App.js
+++ project/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './lib/UserGists';
+
 
 class App extends Component {
+  componentDidMount(){
+    this.fetchData();
+  }
+  
+  fetchData(){
+    fetch('https://api.github.com/users/segdeha/gists')
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(error => console.log('parsing failed', error))
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <p>This is where the search bar will be</p>
+        <button>Search</button>
+        
       </div>
     );
   }
