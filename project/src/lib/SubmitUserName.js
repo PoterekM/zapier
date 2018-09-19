252b3a3befeb5fc4a114c86c34e13b6da6fa54cd
Michelle Poterek <poterekm@gmail.com>
2018-09-19 12:44:58 -0700

    console log for fetching username and name entry work along with receiving array of user gists

--- project/src/lib/SubmitUserName.js
+++ project/src/lib/SubmitUserName.js
@@ -1,33 +1,10 @@
 import React, { Component } from 'react';
 
 class SubmitUserName extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
-  }
+  
 
   render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          User name:
-          <input type="text" placeholder="user name" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
+   
   }
 }
 
--- project/src/lib/UserGists.js
+++ project/src/lib/UserGists.js
@@ -1,11 +1,11 @@
 
 let UserGists = username => {
-    let
     console.log('I am currently fetching', username);
     
     fetch(`https://api.github.com/users/${username}/gists`)
         .then(response => response.json())
         .then(json => console.log(json));
+        
 };
 
 export { UserGists };
